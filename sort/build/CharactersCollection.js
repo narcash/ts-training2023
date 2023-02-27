057cd92be8949b6599ce0464fe16471ed4c65a68
Nar <abylay.kurakbayev@gmail.com>
2023-02-28 01:18:24 +0600

    feat: add interface Soratble

--- sort/build/CharactersCollection.js
+++ sort/build/CharactersCollection.js
@@ -1,1 +1,6 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.CharactersCollection = void 0;
+class CharactersCollection {
+}
+exports.CharactersCollection = CharactersCollection;
